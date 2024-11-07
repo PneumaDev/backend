f34e5ce32ee8e62ff229232c9b1382e39c05aa3d
Eridanus-ui <njigupaul22@gmail.com>
2024-11-07 14:23:53 +0300

    feat: Integrate Cloudinary for image storage
    Connect Cloudinary to the application for image storage and
    management.
    Enable secure image uploading and retrieval via the Cloudinary
    API.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'
 import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // App config
 const app = express()
 const port = process.env.PORT || 4000;
 connectDb()
+connectCloudinary()
 
 // Middlewares
 app.use(express.json())
