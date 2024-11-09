ca41bcb03af4c1a428eba02e784a89b36155a040
Eridanus-ui <njigupaul22@gmail.com>
2024-11-09 16:45:47 +0300

    feat: Implement cart functionality
    Add a new cart route and controller
    Enable users to add items to their cart
    Provide functionality to view and manage cart items

---
+++ controllers/cartController.js
@@ -0,0 +1,21 @@
+
+
+// <---------Add Products To Cart------------->
+const addToCart = async (req, res) => {
+
+}
+
+
+
+// <---------Update User Cart------------->
+const updateCart = async (req, res) => {
+
+}
+
+// <---------Get User Cart------------->
+const getUserCart = async (req, res) => {
+
+}
+
+export { addToCart, updateCart, getUserCart }
+
