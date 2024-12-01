728736964c98bcb73db30f44b9805eaaaaa1d8f3
Eridanus-ui <njigupaul22@gmail.com>
2024-12-01 10:55:43 +0300

    chore: Rename order completion function
    Rename `completePayment` to `confirmPayment` for clarity.
    
    This improves the consistency of naming conventions
    within the order processing functions.

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { placeOrder, placeOrderStripe, userOrders, allOrders, updateStatus, placeOrderMpesa, mpesaWebhook, cancelOrder, completePayment } from "../controllers/orderControllers.js";
+import { placeOrder, placeOrderStripe, userOrders, allOrders, updateStatus, placeOrderMpesa, mpesaWebhook, cancelOrder, confirmPayment } from "../controllers/orderControllers.js";
 import adminAuth from './../middleware/adminAuth.js';
 import authUser from './../middleware/auth.js';
 
@@ -14,7 +14,7 @@ orderRouter.post("/place", authUser, placeOrder)
 orderRouter.post("/stripe", authUser, placeOrderStripe)
 orderRouter.post("/mpesa", authUser, placeOrderMpesa)
 orderRouter.post("/mpesa-webhook", authUser, mpesaWebhook)
-orderRouter.post("/confirmpayment", authUser, completePayment)
+orderRouter.post("/confirmpayment", authUser, confirmPayment)
 
 
 // <----------User Feautures----------->
