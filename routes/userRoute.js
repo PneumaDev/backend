3c2ff51908b6c72da572ddf9821512e8860e7a64
Eridanus-ui <njigupaul22@gmail.com>
2025-02-07 08:03:17 +0300

    ✨ add endpoint to list all users (admin only)

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { loginUser, adminLogin, registerUser } from "../controllers/userController.js";
-import authUser from "../middleware/auth.js";
+import { loginUser, adminLogin, registerUser, getAllUsers } from "../controllers/userController.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
 userRouter.post("/admin", adminLogin)
+userRouter.post("/list", adminAuth, getAllUsers)
 
 
 export default userRouter;
