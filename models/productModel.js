79a13a6590f0cf8ca275fed72b905fcf161302fb
Eridanus-ui <njigupaul22@gmail.com>
2025-02-07 08:03:04 +0300

    🐛 Fix: Set default value for bestSeller to false

--- models/productModel.js
+++ models/productModel.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema({
     category: { type: String, required: true },
     subCategory: { type: String, required: true },
     sizes: { type: Array, required: true },
-    bestSeller: { type: Boolean },
+    bestSeller: { type: Boolean, default: false },
     date: { type: Number, required: true },
     sku: { type: String, unique: true },
     ratings: { type: Number, min: 0, max: 5 },
