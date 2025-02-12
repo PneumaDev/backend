4cecf5bf5474397dfafccd1438a3785da3f6b3ef
Eridanus-ui <njigupaul22@gmail.com>
2025-02-12 08:44:37 +0300

    ⚡️ add text index to product name for faster searches

--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, index: "text" },
     isOriginal: { type: Boolean, required: true },
     quantity: { type: Number, required: true },
     description: { type: String, required: true },
