2a65ab96de453f0e9a3c1ea5794d6ff24a21de38
Eridanus-ui <njigupaul22@gmail.com>
2025-02-12 08:44:50 +0300

    ♻️ Refactor: Change product list route from GET to POST

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,7 +8,7 @@ const productRouter = express.Router();
 productRouter.post('/add', adminAuth, upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 2 }, { name: 'image3', maxCount: 3 }, { name: 'image4', maxCount: 4 }]), addProduct)
 productRouter.post('/remove', adminAuth, removeProduct)
 productRouter.post('/single', singleProductInfo)
-productRouter.get('/list', listProduct)
+productRouter.post('/list', listProduct)
 
 
 export default productRouter;
